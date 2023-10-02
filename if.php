0b7c2fbc97ef0805e05b1b44831919ba221b775d
mackliu <macklun@gmail.com>
2023-10-02 11:15:22 +0800

    PHP基礎-switch-case的使用

--- if.php
+++ if.php
@@ -2,6 +2,8 @@
 
 $score=55;
 
+$score=$score+20;
+
 echo "我的成績:" . $score;
 echo "<br>";
 echo "判斷為:";
@@ -10,4 +12,28 @@ if($score>=60){
     echo "及格";
 }else{
     echo "不及格";
+}
+
+echo "<br>";
+
+$level='C';
+
+echo "成績等級為:".$level;
+echo "<br>";
+
+switch ($level) {
+    case "A":
+        echo "表現優良，請繼續保持";
+    break;
+    case "B":
+        echo "值得肯定，還有進步空間";
+    break;
+    case "C":
+        echo "需要更多的練習";
+    break;
+    case "D":
+        echo "需要加強基本功";
+    break;
+    default:
+        echo "是否無心學業?";
 }
