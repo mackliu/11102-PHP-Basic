4136f07768c8c12eaf88b425a9cf7c0cdb7773ac
mackliu <macklun@gmail.com>
2023-10-06 09:21:43 +0800

    使用迴圈來印出迴圈中變數的變化和程式執行的結果

--- loop.php
+++ loop.php
@@ -1,8 +1,13 @@
 <?php
 $sum=0;
 
-for($i=1;$i<=10000;$i++){
+for($i=1;$i<=10000;$i=$i+1){
     //$sum=$sum+$i;
+    echo '當$i='.$i."時";
+    echo '$sum+$i的結果是';
+    echo $sum . " + ". $i."=";
+    echo $sum+$i;
+    echo "<br>";
     $sum += $i;
 }
 
