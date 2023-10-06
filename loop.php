13339215a8fe9e7d4f8f20bddf84f9910326b7aa
mackliu <macklun@gmail.com>
2023-10-06 14:25:11 +0800

    介紹foreach使用

--- loop.php
+++ loop.php
@@ -92,3 +92,22 @@ for($j=3;$j<=$n;$j++){
 echo "<br>";
 echo "一共執行了".$count."次的程式內容";
 ?>
+<h3>陣列</h3>
+
+<?php
+$a=['甲','乙','丙','丁'];
+
+
+for($i=0;$i<count($a);$i++){
+    echo $a[$i];
+}
+
+echo "<BR>";
+
+foreach($a as $idx => $b){
+    echo $idx ."=>".$b;
+    echo "<br>";
+}
+
+
+?>
