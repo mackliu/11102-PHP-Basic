9f66f6007ae5cc6cb1ac08d4b686758bd5639e64
mackliu <macklun@gmail.com>
2023-10-13 11:04:18 +0800

    九九乘法表-留左下

--- pra02.php
+++ pra02.php
@@ -55,6 +55,40 @@ for($j=0;$j<10;$j++){
 }
 echo "</table>";
 ?>
+<h2>九九乘法表-留左下區</h2>
+<?php
+
+echo "<table>";
+echo "<tr>";
+for($j=0;$j<10;$j++){
+    if($j==0){
+        echo "<tr style='background:#eee'>";
+    }else{
+        echo "<tr>";
+    }
+    for ($i=0;$i<10;$i++){
+        if($i==0){
+            echo "<td style='background:#eee'>";
+        }else{
+            echo "<td>";
+        }
+        if($i==0 && $j==0){
+            echo "";
+        }else if($j==0){
+            echo $i;
+        }else if($i==0){
+            echo $j;
+        }else if($j<$i){
+            echo "";
+        }else {
+            echo $j*$i;
+        }
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
 <!-- 
 <table>
     <tr>
