cc48005201b88a860f2538aa7c62bcf69091336a
mackliu <macklun@gmail.com>
2023-10-13 14:03:10 +0800

    正三角形

--- pra04.php
+++ pra04.php
@@ -53,4 +53,26 @@ for($i=4;$i>=0;$i--){
     echo "<br>";
 }
 
+
+echo "     *";
+echo "<br>";
+echo "    ***";
+?>
+<style>
+    * {
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
+<h2>正三角形</h2>
+<?php
+for($i=0;$i<5;$i++){
+    for($j=0;$j<(4-$i);$j++){
+        echo "&nbsp;";
+    }
+    for($k=0;$k<($i*2+1);$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
 ?>
