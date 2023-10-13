8821545ef518982e2c644bc67c47a4310c33af37
mackliu <macklun@gmail.com>
2023-10-13 15:46:41 +0800

    在星星中加上CSS

--- pra04.php
+++ pra04.php
@@ -147,9 +147,25 @@ for($i=0;$i<7;$i++){
     }
     echo "<br>";
 }
+?>
+<h2>矩形對角線加上css</h2>
+<?php
 
+for($i=0;$i<7;$i++){
 
-
+    for($j=0;$j<7;$j++){
+        if($i==0 || $i==6){
+            echo "*";
+        }else if($j==0 || $j==6){
+            echo "*";
+        }else if($j==$i || $i+$j==6){
+            echo "<span style='color:red'>*</span>";
+        }else{    
+            echo "&nbsp;";
+        }
+    }
+    echo "<br>";
+}
 ?>
 
 
