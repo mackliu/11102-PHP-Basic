13539ea804f4670abf67e84ce38d4b9d19a8ff44
mackliu <macklun@gmail.com>
2023-10-13 13:21:40 +0800

    直角三角形

---
+++ pra04.php
@@ -0,0 +1,44 @@
+<?php
+
+/* echo "*<br>";
+    echo "*";
+    echo "<br>";
+echo "**<br>";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "***<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "****<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "*****<br>";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "*";
+    echo "<br>";
+echo "<hr>";
+$i  $j
+0 < 1
+1   2
+2   3
+3   4
+4   5
+ */
+for($i=0;$i<5;$i++){
+    for($j=0;$j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+
+?>
