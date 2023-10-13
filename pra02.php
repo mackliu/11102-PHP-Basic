14bbe466e0ae956882f79d4354f1017d9eea1b5c
mackliu <macklun@gmail.com>
2023-10-13 10:43:01 +0800

    九九乘法表-變化型

--- pra02.php
+++ pra02.php
@@ -23,6 +23,38 @@ for($j=1;$j<=9;$j++){
 }
 echo "</table>";
 ?>
+<h2>九九乘法表-有標頭</h2>
+<?php
+
+echo "<table>";
+echo "<tr>";
+for($j=0;$j<10;$j++){
+    if($j==0){
+        echo "<tr style='background:#eee'>";
+    }else{
+        echo "<tr>";
+    }
+    for ($i=0;$i<10;$i++){
+        if($i==0){
+            echo "<td style='background:#eee'>";
+        }else{
+            echo "<td>";
+        }
+        if($i==0 && $j==0){
+            echo "";
+        }else if($j==0){
+            echo $i;
+        }else if($i==0){
+            echo $j;
+        }else{
+            echo $j*$i;
+        }
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
+?>
 <!-- 
 <table>
     <tr>
