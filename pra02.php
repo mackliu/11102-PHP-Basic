0150aac65241ae1530880a3c1058b599ea4e8a44
mackliu <macklun@gmail.com>
2023-10-13 09:12:46 +0800

    九九乘法表-巢狀迴圈

---
+++ pra02.php
@@ -0,0 +1,49 @@
+<h2>九九乘法表</h2>
+<?php
+
+for($j=1;$j<=9;$j++){
+    
+    for ($i=1;$i<=9;$i++){
+        
+        echo $j . ' * '.$i.' = '.($j*$i). "<br>";
+    }
+    
+}
+  
+
+/* 
+
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+echo "<br>";
+for ($i=1;$i<=9;$i++){
+    echo '$j * '.$i.' = '.($j*$i). "&nbsp&nbsp&nbsp&nbsp";
+}
+ */
+?>
