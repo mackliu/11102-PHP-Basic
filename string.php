c1bda877b035b0970fcfc4dc53246bce30aeedee
mackliu <macklun@gmail.com>
2023-10-23 16:14:50 +0800

    字串操作示範

--- string.php
+++ string.php
@@ -32,11 +32,59 @@ echo join(' ',$a);
 ?>
 
 <h2>子字串取用</h2>
-
 將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”
+<?php
+echo "<br>";
+$s="The reason why a great man is great is that he resolves to be a great man";
+echo mb_substr($s,0,10);
+echo "...";
+echo "<br>";
+$cs="今天天氣真的是不太好的樣子";
+echo mb_substr($cs,0,3);
+echo "...";
+echo "<br>";
+$cs="今天天氣真的是不太好的樣子";
+echo substr($cs,0,6);
+echo "...";
+
+
+?>
+
+
 <h2>尋找字串與HTML、css整合應用</h2>
 <ul>
     <li>給定一個句子，將指定的關鍵字放大</li>
     <li>“學會PHP網頁程式設計，薪水會加倍，工作會好找”</li>
     <li>請將上句中的 “程式設計” 放大字型或變色.</li>
 </ul>
+
+<?php 
+$key="薪水";
+
+
+$str="學會PHP網頁程式設計，薪水會加倍，工作會好找";
+echo str_replace($key,
+"<span style='font-size:20px;color:red'>$key</span>",
+$str
+);
+
+
+?>
+<?php
+$str="學會PHP網頁程式設計，薪水會加倍，工作會好找";
+for($i=0;$i<mb_strlen($str);$i=$i+3){
+    //echo mb_substr($str,$i,1);
+    echo $str[$i];
+    echo $str[$i+1];
+    echo $str[$i+2];
+    echo "<br>";
+    
+}
+
+
+
+?>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
