e34ac6ee92bc7554b064efec023a293baa3fd86e
mackliu <macklun@gmail.com>
2023-10-24 15:32:00 +0800

    改成以日期為計算基礎的月曆顯示方式

--- calendar.php
+++ calendar.php
@@ -29,6 +29,49 @@ $thisFirstDate=date('w',strtotime($thisFirstDay));
 $thisMonthDays=date("t");
 $thisLastDay=date("Y-m-$thisMonthDays");
 $weeks=ceil(($thisMonthDays+$thisFirstDate)/7);
+$firstCell=date("Y-m-d",strtotime("-$thisFirstDate days",strtotime($thisFirstDay)));
+echo $firstCell;
+echo "<table>";
+echo "<tr>";
+echo "<td>日</td>";
+echo "<td>一</td>";
+echo "<td>二</td>";
+echo "<td>三</td>";
+echo "<td>四</td>";
+echo "<td>五</td>";
+echo "<td>六</td>";
+echo "</tr>";
+for($i=0;$i<$weeks;$i++){
+    echo "<tr>";
+    for($j=0;$j<7;$j++){
+        $addDays=7*$i+$j;
+        $thisCellDate=strtotime("+$addDays days",strtotime($firstCell));
+        if(date('w',$thisCellDate)==0 || date('w',$thisCellDate)==6){
+            echo "<td style='background:pink'>";
+
+        }else{
+            echo "<td>";
+        }
+        if(date("m",$thisCellDate)==date("m",strtotime($thisFirstDay))){
+            echo date("Y-m-d",$thisCellDate);
+        }
+        echo "</td>";
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+?>    
+<?php 
+echo "<h3>";
+echo date("西元Y年m月");
+echo "</h3>";
+$thisMonth=date("Y");
+$thisFirstDay=date("Y-m-1");
+$thisFirstDate=date('w',strtotime($thisFirstDay));
+$thisMonthDays=date("t");
+$thisLastDay=date("Y-m-$thisMonthDays");
+$weeks=ceil(($thisMonthDays+$thisFirstDate)/7);
 
 echo "<table>";
 echo "<tr>";
@@ -55,9 +98,6 @@ for($i=0;$i<$weeks;$i++){
 }
 
 echo "</table>";
-
-
-
 ?>
     <h3>西元2023年10月</h3>
 <table>
